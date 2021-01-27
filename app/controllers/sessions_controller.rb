95a72fd4b59757b9a1097b961380eb7ff9ff2b38
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-01-27 10:53:42 -0300

    working on sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate([:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Logeado correctamente'
+    else
+      flash.now[:notice] = 'Email o password inválida'
+      render action: :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: 'Signed out successfully'
   end
 end
