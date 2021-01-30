aa78d7e7ad87d4982b630222b77d42906d8ad715
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-01-30 10:55:37 -0300

    sessions variables ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,11 +24,13 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(user_params)
 
+    @user = User.new(user_params)
+    
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        session[:user_id] = @user.id
+        format.html { redirect_to root_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
